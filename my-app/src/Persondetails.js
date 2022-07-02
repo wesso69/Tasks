716bb3d1aa51512891f66eba3252120adf63bdac
wesso69 <waseemshraideh@yahoo.com>
2022-07-02 22:31:46 +0300

    third add

--- my-app/src/Persondetails.js
+++ my-app/src/Persondetails.js
@@ -2,29 +2,34 @@ import { useParams } from "react-router-dom";
 import useFetch from "./hooks/useFetsh";
 
 const PersonDetails = () => {
-const {id} = useParams();
-const {data:blog,error,loading} =useFetch('http://icloudjo.com:8050/REST/persons/'+id);
+    const { id } = useParams();
+    const { data: blog, error, loading } = useFetch('http://icloudjo.com:8050/REST/persons/' + id);
+
     return (
-    <div className="person-details"> 
-           {loading && <div>Loading...</div>}
+
+        <div className="person-details">
+            {loading && <div>Loading...</div>}
             {error && <div>{error}</div>}
-            {blog && 
-            <article>
-                <h2> persons {blog.id} </h2>
-                <h2>the  name is: {blog.first_name} {blog.middle_name} {blog.last_name}</h2>
-                <br />
-                <p>  ID:{blog.id}</p>
-                <br />
-                <p>email:{blog.email}</p>
-                <p>description:{blog.description}</p>
-            </article>
-            }
-    </div>
+            {blog && blog.map((blog, id) => (
+                <article key={id}>
+                    <h1>the  name is: {blog.first_name} {blog.middle_name} {blog.last_name}</h1>
+                    <br />
+                    <p>  ID:{blog.id}</p>
+                    <br />
+                    <p>email:{blog.email}</p>
+                    <p>description:{blog.description}</p>
+                </article>
+            ))
+
+
+            };
+        </div>
     );
-  
+
 };
 
 export default PersonDetails;
+
 // // const BlogList = ({ ...props }) => {
 
 //     const { blogs } = props;
@@ -32,7 +37,7 @@ export default PersonDetails;
 //     return (
 //         <div className="blog-list">
 
-//             {blogs.map((blog) => {
+//             {blog.map((blog,index) => {
 //                 return (
 //                     <div className="blog-preview" key={blog.id}>
 //                         <Link to={`/person_details/${blog.id}`}>
